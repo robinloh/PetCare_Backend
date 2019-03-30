d9fad24c8bcca1eb6a32d3bbcaa6516eaf6ed6b7
Robin Loh <robinloh@u.nus.edu>
2019-03-30 17:37:45 +0800

    Update code till Step 7

--- app.js
+++ app.js
@@ -4,8 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/* --- V7: Using dotenv     --- */
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+/* --- V2: Adding Web Pages --- */
+var aboutRouter = require('./routes/about');
+/* ---------------------------- */
+
+/* --- V3: Basic Template   --- */
+var tableRouter = require('./routes/table');
+var loopsRouter = require('./routes/loops');
+/* ---------------------------- */
+
+/* --- V4: Database Connect --- */
+var selectRouter = require('./routes/select');
+/* ---------------------------- */
+
+/* --- V5: Adding Forms     --- */
+var formsRouter = require('./routes/forms');
+/* ---------------------------- */
+
+/* --- V6: Modify Database  --- */
+var insertRouter = require('./routes/insert');
+/* ---------------------------- */
 
 var app = express();
 
@@ -21,6 +45,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/* --- V2: Adding Web Pages --- */
+app.use('/about', aboutRouter);
+/* ---------------------------- */
+
+/* --- V3: Basic Template   --- */
+app.use('/table', tableRouter);
+app.use('/loops', loopsRouter);
+/* ---------------------------- */
+
+/* --- V4: Database Connect --- */
+app.use('/select', selectRouter);
+/* ---------------------------- */
+
+/* --- V5: Adding Forms     --- */
+app.use('/forms', formsRouter);
+/* ---------------------------- */
+
+/* --- V6: Modify Database  --- */
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/insert', insertRouter);
+/* ---------------------------- */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
