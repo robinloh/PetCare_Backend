33110933ae79b2bf9ee4937d1d5fc90445c12261
Robin Loh <robinloh@u.nus.edu>
2019-04-10 20:01:01 +0800

    Update getAvailabilities query

--- models/queries.js
+++ models/queries.js
@@ -25,7 +25,7 @@ queries.query = {
     // Availability and bids related
     add_availability: 'INSERT INTO Availabilities VALUES($1, $2, $3, $4) RETURNING startDate, endDate', //[startDate, endDate, email, autoAcceptedPrice]
     delete_availability: 'DELETE FROM Availabilities WHERE startDate = $1 && endDate = $2 && email = $3', //[startDate, endDate, email]
-    get_availability: 'SELECT TO_CHAR(startDate, \'YYYY-MM-DD\'), TO_CHAR(endDate, \'YYYY-MM-DD\'), autoAcceptedPrice FROM Availabilities WHERE email = $1', //[email]
+    get_availability: 'SELECT TO_CHAR(startDate, \'YYYY-MM-DD\') AS startDate, TO_CHAR(endDate, \'YYYY-MM-DD\') As endDate, autoAcceptedPrice FROM Availabilities WHERE email = $1', //[email]
 
     find_services: 'SELECT email FROM Availabilities A INNER JOIN provideService S WHERE $1 > startDate AND $1 < endDate AND serviceid = $2 AND NOT EXISTS (SELECT 1 FROM Bids B WHERE A.email = B.caretakerEmail AND dateOfService = $1 AND status = "Won")', // [dateOfService, typeOfService]
     get_work_schedule: 'SELECT DateOfService, bidderEmail, bidAmount FROM Bids WHERE caretakerEmail = $1 and status = "Won"', //[caretakerEmail]
