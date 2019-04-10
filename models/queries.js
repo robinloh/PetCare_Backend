bcd9c51a88ec8b89fe625b01900f8b97230cfbd2
Robin Loh <robinloh@u.nus.edu>
2019-04-11 02:20:34 +0800

    Fix PetSection

--- models/queries.js
+++ models/queries.js
@@ -18,9 +18,9 @@ queries.query = {
 
     // Pet related
     add_pet: 'INSERT INTO Pets VALUES($1) RETURNING pid', //[name] pid to be generated
-    delete_pet: 'DELETE FROM Pets WHERE pid = $1',
+    delete_pet: 'DELETE FROM Pets WHERE pid = $1 RETURNING pid',
     add_pets_owner: 'INSERT INTO OwnsPet VALUES($1, $2)', //[email, pid]
-    get_all_pets_from_petowner: 'SELECT pid FROM getPetsInfo WHERE email = $1', // [pid]
+    get_all_pets_from_petowner: 'SELECT name, breedname, speciesname, diet, specialnote FROM getPetsInfo WHERE email = $1', // [name, breedName, speciesName, diet, specialNote]
 
     // Availability and bids related
     add_availability: 'INSERT INTO Availabilities VALUES($1, $2, $3, $4) RETURNING startDate, endDate', //[email, startDate, endDate, autoAcceptedPrice]
--- routes/petsection.js
+++ routes/petsection.js
@@ -54,7 +54,7 @@ router.post('/', function (req, res, next) {
                     console.log("\nADD PET SUCCESSFUL\n");
                     console.log(data);
 
-                    res.send("success " + data.pid);
+                    res.send(data);
 
                 } catch (e) {
                     await client.query('ROLLBACK')
@@ -67,29 +67,52 @@ router.post('/', function (req, res, next) {
             break;
 
         case "deletePets":
-            pool.query(queries.query.delete_pet, [data.pid], (err, result1) => {
-                if (err) {
-                    res.status(400).send(err.stack);
+
+            (async () => {
+                const client = await pool.connect()
+                try {
+                    await client.query('BEGIN')
+
+                    // Delete pet
+                    const {rows} = await client.query(queries.query.delete_pet, [data.pid])
+                    data.pid = rows[0].pid;
+
+                    console.log("\nDELETE PET SUCCESSFUL\n");
+                    res.send(data);
+
+                } catch (e) {
+                    await client.query('ROLLBACK')
+                    throw e
+                } finally {
+                    client.release()
                 }
-                pool.query(queries.query.get_all_pets_from_petowner, [data.email], (err, result2) => {
-                    if (err) {
-                        res.status(400).send(err.stack);
-                    }
-                    console.log(result2);
-                    res.send(result2);
-                });
-            });
+            })().catch(e => {console.error(e.message)
+                res.status(400).send(e.message)})
             break;
         
         case "getAllPets":
-            pool.query(queries.query.get_all_pets_from_petowner, [data.email], (err, result) => {
-                if (err) {
-                    res.status(400).send(err.stack);
-                } else {
-                    console.log(result);
-                    res.send(result);
+            (async () => {
+                const client = await pool.connect()
+                try {
+                    await client.query('BEGIN')
+
+                    // Get All Pets
+                    const result = await client.query(queries.query.get_all_pets_from_petowner, [data.email])
+
+                    await client.query('COMMIT')
+
+                    console.log("\nGET PET SUCCESSFUL\n");
+                    console.log(result.rows);
+                    res.send(result.rows);
+
+                } catch (e) {
+                    await client.query('ROLLBACK')
+                    throw e
+                } finally {
+                    client.release()
                 }
-            });
+            })().catch(e => {console.error(e.message)
+                res.status(400).send(e.message)})
             break;
 
         case "getAllSpecies":
