de802fb012887d9e199397f48e54c93ee7dc9cdc
Fyelight <kaid.fyelight@hotmail.com>
2019-04-11 18:58:16 +0800

    Update acceptBid

--- routes/caretaker.js
+++ routes/caretaker.js
@@ -196,12 +196,12 @@ router.post('/', function (req, res, next) {
                     const tempResult = await client.query(queries.query.accept_bid, [data.bid])
                     const dateOfService = tempResult.rows[0].dateOfService
                     const caretakerEmail = tempResult.rows[0].caretakerEmail
-                    await client.query(queries.query.delete_availability, [dateOfService])
+                    await client.query(queries.query.delete_availability, [caretakerEmail, dateOfService])
                     await client.query('COMMIT')
                     const availResult = await client.query(queries.query.get_availability, [caretakerEmail])
                     const workDateResult = await client.query(queries.query.get_work_schedule, [caretakerEmail])
                     console.log("availabilities: \n" + availResult.rows + "\n work dates: \n" + workDateResult.rows);
-                    res.send("availabilities: \n" + availResult.rows + "\n work dates: \n" + workDateResult.rows);
+                    res.send({availabilities: availResult.rows, workdates: workDateResult.rows});
                 } catch (e) {
                     await client.query('ROLLBACK')
                     throw e
